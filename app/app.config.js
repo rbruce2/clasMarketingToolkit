d8ab064beb392da86c6438031a5a999c615f59ee
Robert Bruce <robertbruce@clas-robert-bruce-imac.dhcp.asu.edu>
2017-05-03 15:01:44 -0700

    initial commit

---
+++ app/app.config.js
@@ -0,0 +1,69 @@
+(function () {
+	'use strict';
+
+	/**
+	 * @ngdoc configuration file
+	 * @name app.config:config
+	 * @description
+	 * # Config and run block
+	 * Configutation of the app
+	 */
+
+
+	angular
+		.module('clas-marketing-tool-kit')
+		.config(configure)
+		.run(runBlock);
+
+	configure.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', '$mdThemingProvider'];
+
+	function configure($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $mdThemingProvider) {
+
+		// $mdThemingProvider.theme('default').primaryPalette('pink').accentPallete('orange');
+		$mdThemingProvider.definePalette('asuPalette', {
+    '50': 'ffebee',
+    '100': 'ffcdd2',
+    '200': 'ef9a9a',
+    '300': 'e57373',
+    '400': 'ef5350',
+    '500': 'f44336',
+    '600': 'e53935',
+    '700': 'd32f2f',
+    '800': '353535',
+    '900': 'b71c1c',
+    'A100': 'ff8a80',
+    'A200': 'ff5252',
+    'A400': 'ff1744',
+    'A700': 'd50000',
+    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                        // on this palette should be dark or light
+
+    'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+     '200', '300', '400', 'A100'],
+    'contrastLightColors': undefined    // could also specify this if default was 'dark'
+  });
+
+  $mdThemingProvider.theme('default')
+    .primaryPalette('asuPalette');
+
+		$locationProvider.hashPrefix('!');
+
+		// This is required for Browser Sync to work poperly
+		$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
+		$urlRouterProvider
+			.otherwise('/dashboard');
+
+	}
+
+	runBlock.$inject = ['$rootScope'];
+
+	function runBlock($rootScope) {
+		'use strict';
+
+		console.log('AngularJS run() function...');
+	}
+
+
+})();
---
+++ app/app.js
@@ -0,0 +1,26 @@
+(function() {
+	'use strict';
+
+	/**
+	* @ngdoc index
+	* @name app
+	* @description
+	* # app
+	*
+	* Main module of the application.
+	*/
+
+	angular.module('clas-marketing-tool-kit', [
+		'ngResource',
+		'ngAria',
+		 'ngMaterial',
+		'ngMdIcons',
+		'ngMessages',
+		'ngCookies',
+		'ngAnimate',
+		'ngSanitize',
+		'ui.router',
+		'home',
+	]);
+
+})();
