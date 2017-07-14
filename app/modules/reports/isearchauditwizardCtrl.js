414c9f7d5ae295aaa58c06859b646c5eeb7a7336
Robert Bruce <rbruce2@asu.edu>
2017-07-14 13:42:37 -0700

    added isearchauditwizard endpoint

---
+++ app/modules/reports/isearchauditwizardCtrl.js
@@ -0,0 +1,36 @@
+(function() {
+	'use strict';
+
+	/**
+	* @ngdoc function
+	* @name app.controller:isearchauditwizardCtrl
+	* @description
+	* # isearchauditwizardCtrl
+	* Controller of the app
+	*/
+
+  	angular
+		.module('reports')
+		.controller('isearchauditwizardCtrl', iSearchAuditWizard);
+
+		iSearchAuditWizard.$inject = ['ReportsService', '$rootScope', '$http', '$location'];
+
+		/*
+		* recommend
+		* Using function declarations
+		* and bindable members up top.
+		*/
+
+		function iSearchAuditWizard(ReportsService, $rootScope, $http, $location) {
+			/*jshint validthis: true */
+			var vm = this;
+			vm.params = $location.search();
+			console.log($location.search());
+
+
+
+		} //end iSearchAuditWizardCtrl function
+
+		// helper functions
+
+})();
